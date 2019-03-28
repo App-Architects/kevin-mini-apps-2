4e29e798d6a0ff9ce50001b660d062f8da126f31
Kevin Haiduy Phung <kphung29@gmail.com>
2019-03-27 22:00:55 -0400

    styling for chart component

--- challenge_2/client/src/App.js
+++ challenge_2/client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import { Grid, GridColumn, GridRow } from 'semantic-ui-react';
+
 import Chart from './components/Chart';
 
 const App = () =>  (
-  <div>
-    <Chart />
-  </div>
+
+  <Grid style={ {paddingLeft: 20 }} columns={2} >
+    <GridRow>
+      <GridColumn width={2} textAlign='center'>
+      </GridColumn>
+      <GridColumn width={12}>
+        <Chart />
+      </GridColumn>
+    </GridRow>
+  </Grid>
 )
 
 export default App;
